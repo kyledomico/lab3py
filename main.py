a19891331b45fce611222957def8b3d76856bd1d
kyledomico <kdd5262@psu.edu>
2020-09-15 13:43:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+# Author: Kyle Domico kdd5262@psu.edu
+# Collaborator: Travis Stauffer tcs5399@psu.edu
+# Collaborator: Teya Davis tmd5681@psu.edu
+# Collaborator: Shengyou Jiang sjj5492@psu.edu
+# Section: 1
+# Breakout: 2
+
+def sum_n(n):
+  total = 0
+  if n < 0:
+    return total
+  else:
+    total = n + sum_n(n-1)
+  return total
+
+def print_n(s,n):
+  if n == 1:
+    return print(s)
+  else:
+    print(s)
+    print_n(s,n-1)
+  return
+
+def run():
+  n = int(input("Enter an int: "))
+  print(f"sum is {sum_n(n)}")
+  s = input("Enter a string: ")
+  print_n(s,n)
+
+if __name__ == "__main__":
+  run()
+
+    
+
